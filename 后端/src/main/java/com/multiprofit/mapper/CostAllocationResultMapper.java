08375883e405b7f33e8b81971cd6fb9904983693
zhaozhun <764030100@qq.com>
2026-06-27 18:40:39 +0800

    feat: 创建指标汇总和费用分摊结果Mapper接口

---
+++ 后端/src/main/java/com/multiprofit/mapper/CostAllocationResultMapper.java
@@ -0,0 +1,9 @@
+package com.multiprofit.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.multiprofit.entity.CostAllocationResult;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface CostAllocationResultMapper extends BaseMapper<CostAllocationResult> {
+}
---
+++ 后端/src/main/java/com/multiprofit/mapper/IndicatorSummaryMapper.java
@@ -0,0 +1,9 @@
+package com.multiprofit.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.multiprofit.entity.IndicatorSummary;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface IndicatorSummaryMapper extends BaseMapper<IndicatorSummary> {
+}
